b5798f8214a6a17b1c59810deb7428c0aa39b7f8
Gabriel Sorensen <g.soren.sen2004@gmail.com>
2025-11-08 12:58:45 -0300

    V1.0 dos models criados

--- backend/models/GameMatch.php
+++ backend/models/GameMatch.php
@@ -1,17 +1,57 @@
 <?php
 require_once __DIR__ . '/../config/database.php';
 
-class GameMatch 
+class GameMatch
 {
 
+    public function saveGame($matchData)
+    {
+
+        global $pdo;
+
+        $saveGameSQL = "INSERT INTO partidas (user_id, dimensao, modalidade, tempo_gasto, jogadas, resultado)
+                VALUES (?, ?, ?, ?, ?, ?)";
 
 
+        $stmt = $pdo->prepare($saveGameSQL);
+
+        return $stmt->execute([
+            $matchData['user_id'],
+            $matchData['dimensao'],
+            $matchData['modalidade'],
+            $matchData['tempo_gasto'],
+            $matchData['jogadas'],
+            $matchData['resultado']
+        ]);
+    }
+
+    public function searchUserMatchs($userId)
+    {
+
+        global $pdo;
+
+        $searchUserSQL = "SELECT * FROM partidas where user_id = ? ORDER BY data_parida DESC";
+
+        $stmt = $pdo->prepare($searchUserSQL);
+        $stmt->execute([$userId]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
 
+    public function topMatchs()
+    {
 
+        global $pdo;
 
+        $topMatchSQL = "SELECT u.username, p.*
+                FROM partidas p
+                JOIN users u ON u.id = p.user_id
+                WHERE p.resultado = 'Vitoria'
+                ORDER BY p.dimensao DESC, p.jogadas ASC, p.tempo_gasto ASC
+                LIMIT 10";
 
+        $stmt = $pdo->query($topMatchSQL);
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
+    }
 }
