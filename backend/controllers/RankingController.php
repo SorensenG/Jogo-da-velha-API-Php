aa615f1f299f73e842300f70995e38d2df5edeb6
Gabriel Sorensen <g.soren.sen2004@gmail.com>
2025-11-09 12:34:54 -0300

    criando contollers, controller de ranking feito

--- backend/controllers/RankingController.php
+++ backend/controllers/RankingController.php
@@ -1,1 +1,27 @@
- 
+<?php
+require_once __DIR__ . '/../models/GameMatch.php';
+
+class RankingController
+{
+
+    public function getTopPlayers()
+    {
+
+        $match = new GameMatch();
+
+        $topPlayers = $match->topPlayers();
+
+        if ($topPlayers && count($topPlayers) > 0) {
+
+            return [
+                'status' => '200 ok',
+                'message' => 'top 10 players ranked sucessefully',
+            ];
+        } else {
+            return [
+                'status' => '404 not faund',
+                'message' => 'Sem jogadores no ranking'
+            ];
+        }
+    }
+}
